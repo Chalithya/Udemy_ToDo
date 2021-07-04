b952fde5e6aa93a733051ddfd1c3eaa2035ff7cc
Chalithya Sangeeth <chalithyaofficial@gmail.com>
2021-07-04 10:04:37 +0530

    ready to isntall axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./components/Form";
 import Section from "./components/Section";
 import List from "./components/List";
+import axios from "axios";  
 
 const appTitle = "To-Do App";
 const list = [
@@ -15,6 +16,14 @@ const list = [
 const App = () => {
 
   const [todoList, setTodoList] = useState(list);
+
+  useEffect(()=>{
+    async function fetchData(){
+    const response = await axios.get("http://localhost:3030/todos");
+    }
+    fetchData();
+
+  }, []);
 
   const addTodo = (item) => {
     setTodoList((oldList)=>[...oldList, item]);
